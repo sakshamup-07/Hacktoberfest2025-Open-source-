6f8229d108f41ccc3b2ba736fca1d6ad46d258fe
Saksham Upadhyay <154794993+sakshamup-07@users.noreply.github.com>
2025-10-06 04:12:56 +0530

    Create Fibo.cpp

---
+++ Fibo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if (n <= 1) return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+    for (int i = 0; i < 10; i++) 
+        cout << fib(i) << " ";
+}
